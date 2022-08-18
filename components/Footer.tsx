3cdf88e0755d5523edface513c8a6e04ce5e6809
nurzhanm2160 <nurzhan.murzakhmet@gmail.com>
2022-08-18 15:18:21 +0600

    Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,29 @@
 import React from 'react'
 
+import { footerList1, footerList2, footerList3 } from '../utils/constants'
+
+const List = ({items, mt}: {items: string[], mt: boolean}) => {
+  return (
+    <div className={`flex flex-wrap gap-2 ${mt && "mt-5" }`}>
+      {items.map((item) => (
+        <p key={item} className="text-gray-400 text-sm hover:underline curosr-pointer">
+          {item}
+        </p>
+      ))}
+    </div>
+  )
+}
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='mt-6 hidden xl:block'>
+      <List items={footerList1} mt={false} /> 
+      <List items={footerList2} mt={true} />
+      <List items={footerList3} mt={true} />
+      <p>
+        2022 TikTok clone 
+      </p>
+    </div>
   )
 }
 
