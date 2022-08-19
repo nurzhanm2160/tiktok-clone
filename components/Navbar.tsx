18ed7c0b839fd8725b24325e4cf7b0278db1ea54
nurzhanm2160 <nurzhan.murzakhmet@gmail.com>
2022-08-19 23:21:21 +0600

    added: oAuth, zustand store.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,14 +2,19 @@ import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-import { GoogleLogin, GoogleLogout } from 'react-google-login'
 import { AiOutlineLogout } from 'react-icons/ai'
 import { BiSearch } from 'react-icons/bi'
 import {IoMdAdd} from 'react-icons/io'
+import { GoogleLogin, googleLogout } from '@react-oauth/google'
 
 import Logo from '../utils/tiktik-logo.png'
+import { createOrGetUser } from '../utils'
+
+import useAuthStore from '../store/authStore'
 
 const Navbar = () => {
+  const { userProfile, addUser } = useAuthStore()
+
   return (
     <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
       <Link href="/">
@@ -21,6 +26,20 @@ const Navbar = () => {
           />
         </div>
       </Link>
+
+      <div>SEARCH</div>
+
+      <div>
+        {userProfile ? (
+          <div>{userProfile?.userName}</div>
+        )
+        :
+        <GoogleLogin 
+          onSuccess={response => createOrGetUser(response, addUser)}
+          onError={() => console.log('error')}
+        />
+        }
+      </div>
     </div>
   )
 }
--- pages/api/post/index.ts
+++ pages/api/post/index.ts
@@ -12,5 +12,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const data = await client.fetch(query)
 
     res.status(200).json(data)
+
   }
 }
