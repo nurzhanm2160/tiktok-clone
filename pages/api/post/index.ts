7b2af98971b531974052215b607643116d3fe3d4
nurzhanm2160 <nurzhan.murzakhmet@gmail.com>
2022-10-11 01:39:57 +0600

    adding video, fixing bugs

--- pages/api/post/index.ts
+++ pages/api/post/index.ts
@@ -13,5 +13,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
     res.status(200).json(data)
 
+  } else if (req.method === 'POST') {
+    const document = req.body
+
+    client.create(document).then(() => res.status(200).json('Video Created'))
   }
 }
