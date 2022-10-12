f61feae6af785a853db39a0e197500c41a28bdba
nurzhanm2160 <nurzhan.murzakhmet@gmail.com>
2022-10-12 21:56:43 +0600

    Изменения для деплоя приложения

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,13 @@
-import type { AppProps } from 'next/app';
-import { useEffect, useState } from 'react';
-import { GoogleOAuthProvider } from '@react-oauth/google';
+import type {AppProps} from 'next/app';
+import {useEffect, useState} from 'react';
+import {GoogleOAuthProvider} from '@react-oauth/google';
+
 
 import Navbar from '../components/Navbar';
 import Sidebar from '../components/Sidebar';
 import '../styles/globals.css';
 
-const MyApp = ({ Component, pageProps }: AppProps) => {
+const MyApp = ({Component, pageProps}: AppProps) => {
     const [isSSR, setIsSSR] = useState(true);
 
     useEffect(() => {
@@ -18,10 +19,10 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
     return (
         <GoogleOAuthProvider clientId={`${process.env.NEXT_PUBLIC_GOOGLE_API_TOKEN}`}>
             <div className='xl:w-[1200px] m-auto overflow-hidden h-[100vh]'>
-                <Navbar />
+                <Navbar/>
                 <div className='flex gap-6 md:gap-20 '>
                     <div className='h-[92vh] overflow-hidden xl:hover:overflow-auto'>
-                        <Sidebar />
+                        <Sidebar/>
                     </div>
                     <div className='mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1'>
                         <Component {...pageProps} />
